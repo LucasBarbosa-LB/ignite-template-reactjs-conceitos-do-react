61406c26dcd0ab5e142e44819686679798e54ae6
Lucas Barbosa <59850316+LucasBarbosa-LB@users.noreply.github.com>
2021-10-02 10:17:55 -0300

    testes finalizados

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    const newTask = {
+      id:Math.random() * 10,
+      title:newTaskTitle,
+      isComplete:false
+    }
+    setTasks([...tasks,newTask])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTask = tasks.map(task => id === task.id ? {
+      ...task,
+      isComplete:!task.isComplete
+    }: task)
+    setTasks(newTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks)
   }
 
   return (
@@ -62,7 +77,7 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={30}/>
               </button>
             </li>
           ))}
